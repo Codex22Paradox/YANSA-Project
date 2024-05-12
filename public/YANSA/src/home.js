b459bde74208f2252ef1ec13a47e46e5c13294e7
MattiaGrazianoPedicini <pedicinimattiagraziano@itis-molinari.eu>
2024-05-12 13:02:20 +0200

    redirect se non hai accesso

--- public/YANSA/src/home.js
+++ public/YANSA/src/home.js
@@ -1,54 +1,58 @@
 window.onload = async function () {
+  if (sessionStorage.getItem("token") !== null) {
     await render(document.getElementById("noteContainer"));
+  } else {
+    window.location.href = "./accedi.html";
+  }
 };
 
 const pickData = async () => {
-    let rsp = await fetch("/userFeed", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: sessionStorage.getItem("token"),
-        },
-    });
-    rsp = await rsp.json();
-    //console.log(rsp);
-    return rsp;
+  let rsp = await fetch("/userFeed", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: sessionStorage.getItem("token"),
+    },
+  });
+  rsp = await rsp.json();
+  //console.log(rsp);
+  return rsp;
 };
 
 const pickComponent = async (url) => {
-    let rsp = await fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: sessionStorage.getItem("token"),
-        },
-    });
-    rsp = await rsp.json();
-    rsp = JSON.parse(rsp.Result);
-    //console.log(rsp);
-    return rsp;
+  let rsp = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: sessionStorage.getItem("token"),
+    },
+  });
+  rsp = await rsp.json();
+  rsp = JSON.parse(rsp.Result);
+  //console.log(rsp);
+  return rsp;
 };
 
 const prova = async () => {
-    let value = await pickData();
-    const tutto = [];
-    for (let index = 0; index < value.length; index++) {
-        let obj = {
-            titolo: value[index].nome,
-        };
-        let res = await pickComponent("/getNote/" + value[index].nome);
-        obj["data"] = res.dateCreation;
-        obj["autore"] = res.author;
-        let sum = "";
-        res.data.blocks.forEach((element) => {
-            if (element.type === "paragraph") {
-                sum += element.data.text;
-            }
-        });
-        obj["contenuto"] = sum;
-        tutto.push(obj);
-    }
-    return tutto;
+  let value = await pickData();
+  const tutto = [];
+  for (let index = 0; index < value.length; index++) {
+    let obj = {
+      titolo: value[index].nome,
+    };
+    let res = await pickComponent("/getNote/" + value[index].nome);
+    obj["data"] = res.dateCreation;
+    obj["autore"] = res.author;
+    let sum = "";
+    res.data.blocks.forEach((element) => {
+      if (element.type === "paragraph") {
+        sum += element.data.text;
+      }
+    });
+    obj["contenuto"] = sum;
+    tutto.push(obj);
+  }
+  return tutto;
 };
 let template = `        
     <div class="col-auto mt-3">
@@ -75,45 +79,42 @@ let template = `
     </div>
   </div>`;
 const render = async (div) => {
-    div.innerHTML = "";
-    const listino = await prova();
-    let output = "";
-    let controllino = 1;
-    listino.forEach((appunto) => {
-        let troncatura = "";
-        if (controllino === 1) {
-            troncatura = "truncate-md";
-        } else if (controllino === 2) {
-            troncatura = "truncate-xl";
-        } else if (controllino === 3) {
-            troncatura = "truncate-sm";
-        }
-        let html = "";
-        html = template.replace("%TITOLO", appunto.titolo);
-        html = html.replace("%AUTORE", appunto.autore);
-        html = html.replace(
-            "%DATA",
-            dataIta(appunto.data.substring(0, 10))
-        );
-        html = html.replace("%CONTENUTO", appunto.contenuto);
-        html = html.replace("%TRONCA", troncatura);
-        output += html;
-        if (controllino === 1) {
-            controllino = 2;
-        } else if (controllino === 2) {
-            controllino = 3;
-        } else if (controllino === 3) {
-            controllino = 1;
-        }
-    });
-    div.innerHTML = output;
+  div.innerHTML = "";
+  const listino = await prova();
+  let output = "";
+  let controllino = 1;
+  listino.forEach((appunto) => {
+    let troncatura = "";
+    if (controllino === 1) {
+      troncatura = "truncate-md";
+    } else if (controllino === 2) {
+      troncatura = "truncate-xl";
+    } else if (controllino === 3) {
+      troncatura = "truncate-sm";
+    }
+    let html = "";
+    html = template.replace("%TITOLO", appunto.titolo);
+    html = html.replace("%AUTORE", appunto.autore);
+    html = html.replace("%DATA", dataIta(appunto.data.substring(0, 10)));
+    html = html.replace("%CONTENUTO", appunto.contenuto);
+    html = html.replace("%TRONCA", troncatura);
+    output += html;
+    if (controllino === 1) {
+      controllino = 2;
+    } else if (controllino === 2) {
+      controllino = 3;
+    } else if (controllino === 3) {
+      controllino = 1;
+    }
+  });
+  div.innerHTML = output;
 };
 
 const dataIta = (dataEstera) => {
-    let data = new Date(dataEstera);
-    let giorno = data.getDate();
-    let mese = data.getMonth() + 1;
-    let anno = data.getFullYear();
-    let dataItaliana = giorno + "/" + mese + "/" + anno;
-    return dataItaliana;
+  let data = new Date(dataEstera);
+  let giorno = data.getDate();
+  let mese = data.getMonth() + 1;
+  let anno = data.getFullYear();
+  let dataItaliana = giorno + "/" + mese + "/" + anno;
+  return dataItaliana;
 };
