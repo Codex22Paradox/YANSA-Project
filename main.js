3e1be4910cc25364471def8d344d8d0a5cea6855
FapaKslapa <stefanomarocco0@gmail.com>
2025-01-17 14:19:26 +0100

    Fix port

--- main.js
+++ main.js
@@ -45,8 +45,8 @@ app.use((req, res, next) => {
         });
     }
 });/**/
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3003, () => {
+    console.log('Server is running on port 3003');
 });
 
 //Metodi post
